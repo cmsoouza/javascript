4d16c863bf673028d8d492025b7816051899a9b9
Cristian Souza <cmsouzaac@outlook.com>
2022-02-12 08:23:17 -0500

    total

---
+++ fundamentos/readme.md
@@ -0,0 +1,4 @@
+## <h2> Fundamentos
+
+- [Tipos de Dados Boolean](https://javascript.info/types#boolean-logical-type)
+- [Operadores Booleanos](https://javascript.info/logical-operators)
--- logica/operadores/logico_2.js
+++ logica/operadores/logico_2.js
@@ -1,4 +1,4 @@
 const t1 = true;
 const t2 = true;
 
-console.log('Vamos comprar a TV 50"?' , t1 && t2);//exibindo uma ' "" ' (aspa simples)
+console.log('Vamos comprar a TV 50"?' , t1 && t2);
