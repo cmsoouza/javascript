dd447a2bb74b0334254f0d7d77b81cd01e0474ee
Cristian Souza <cmsouzaac@outlook.com>
2022-03-25 10:27:46 -0500

    atualização nullundefined

--- fundamentos/nullUndefined.js
+++ fundamentos/nullUndefined.js
@@ -1,6 +1,17 @@
+//1) undefined valor indefinido
 let valor
 console.log(valor);
 
-//1) ausencia de valor
+//2) ausencia de valor
 valor = null
 console.log(valor); 
+/* console.log(valor.toString()) erro! */ 
+
+const produto = {}
+console.log(produto.preco);
+
+produto.preco = 3.50
+console.log(produto);
+
+produto.preco = undefined
+console.log(!!produto.preco);
