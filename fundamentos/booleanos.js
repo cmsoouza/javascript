29562c2ddc8c84cbf5a6be769cb09e0b06def362
Cristian <cmsouzaac@outlook.com>
2022-04-12 12:49:36 -0300

    atualizações

---
+++ fundamentos/booleanos.js
@@ -0,0 +1,34 @@
+let nameFieldChechekd = true
+let ageFieldChechekd = false
+
+console.log(nameFieldChechekd);
+console.log(ageFieldChechekd);
+
+//Em comparações de valores 
+let isGreater = 4 < 1
+let letGreater = 4 > 1
+
+console.log(isGreater);
+console.log(letGreater);
+
+//operadores logicos
+
+let comparacao1 = (true || true)
+let comparacao2 = (false || false)
+let comparacao3 = (true || false)
+let comparacao4 = (false || true)
+
+
+console.log(comparacao1);
+console.log(comparacao2);
+console.log(comparacao3);
+console.log(comparacao4);
+
+//exemplo usando instrução if()
+let horas = 9
+if (horas < 10 || horas > 18) {
+    return horas
+}/* primeiro operando = true, false (Avalia operandos da esquerda para a direita.) */
+
+console.log(horas);
+
