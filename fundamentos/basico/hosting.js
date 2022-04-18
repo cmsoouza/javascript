c499eba0229fac3a80584fd75f09f99bb3efbd0b
Cristian <cmsouzaac@outlook.com>
2022-04-18 01:02:15 -0300

    at_hoisting

--- fundamentos/basico/hosting.js
+++ fundamentos/basico/hosting.js
@@ -13,14 +13,6 @@ function teste() {
 teste();
 console.log('a = ', a);
 
-//Tipos let e const
-carName = 'volvo'
-let carName;
-console.log(carName);
-
-carColor = 'red'
-//const carColor
-
 /**
  * anotações;
  * 1.   O termo hosting é utilizado para definir o conceito em que variaveis durante a fase de compilação
@@ -29,6 +21,7 @@ carColor = 'red'
  * 2.   Em js uma variavel pode ser declarada apos ou antes de sua utilização.
  * 
  * 3.   Usando let e const antes de ser declaradas resultará em erro. Variavel se encontra numa "zona morta'
+ *      isso quer dizer que não pode ser acessada fora da do escopo da função.
  * 
  * 4.   O hosting funciona bem como outro tipos de dados e variaveis. Variaveis podem ser inicializadas e usadas
  *      antes de serem declaraadas. Para evitar bugs sempre declare as variaveis antes de serem usadas no inicio
