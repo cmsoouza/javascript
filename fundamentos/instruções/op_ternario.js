968a39d008e26014020ff0078c3ec21c7b6463cc
Cristian <cmsouzaac@outlook.com>
2022-04-28 15:15:26 -0300

    intruções

---
+++ fundamentos/instruções/op_ternario.js
@@ -0,0 +1,13 @@
+const javascript = false;
+
+console.log(
+    javascript ? 'verdadeiro' : "falso"
+);
+
+/**
+ * anotação:
+ * 1.   ?:  é o operador ternário
+ * 
+ * 2.   Nessa instrução se a condição for verdadeira, irá imprimir no console
+ *      "verdadeiro", e se for falsa, irá imprimir "falso".
+ */
